71e566a67727846f78cb7a0a04f48a4ff0234777
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2026-04-26 09:49:32 +0200

    Met à jour les badges d'étapes avec intitulés complets

--- tests/QslHelpersExtendedTest.php
+++ tests/QslHelpersExtendedTest.php
@@ -6,6 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 final class QslHelpersExtendedTest extends TestCase
 {
+    public function testQslNormalizeQslStatusKeepsKnownStatusesOnly(): void
+    {
+        self::assertSame('Y', qsl_normalize_qsl_status(' y '));
+        self::assertSame('R', qsl_normalize_qsl_status('received'));
+        self::assertSame('', qsl_normalize_qsl_status('X'));
+    }
+
     public function testQslNormalizeCallsignRemovesInvalidCharsAndSlashes(): void
     {
         self::assertSame('ON4CRD', qsl_normalize_callsign('/ on4@crd //'));
@@ -42,6 +49,20 @@ ADIF;
         self::assertSame('TNX 73', $rows[1]['comment']);
     }
 
+    public function testParseAdifReadsEqslAndLotwStatuses(): void
+    {
+        $adif = <<<ADIF
+<CALL:6>ON4ABC<QSO_DATE:8>20260412<TIME_ON:4>0915<EQSL_QSL_SENT:1>Y<EQSL_QSL_RCVD:1>R<LOTW_QSL_SENT:1>N<LOTW_QSL_RCVD:1>Y<EOR>
+ADIF;
+
+        $rows = parse_adif($adif);
+        self::assertCount(1, $rows);
+        self::assertSame('Y', $rows[0]['eqsl_qsl_sent'] ?? '');
+        self::assertSame('R', $rows[0]['eqsl_qsl_rcvd'] ?? '');
+        self::assertSame('N', $rows[0]['lotw_qsl_sent'] ?? '');
+        self::assertSame('Y', $rows[0]['lotw_qsl_rcvd'] ?? '');
+    }
+
     public function testSanitizeSvgDocumentReturnsSafeFallbackForDangerousMarkup(): void
     {
         $dangerous = '<svg><image onload="evil()"></image></svg>';
@@ -71,6 +92,16 @@ ADIF;
     {
         $safe = '<svg xmlns="http://www.w3.org/2000/svg"><text>ON4CRD</text></svg>';
         self::assertSame($safe, sanitize_svg_document($safe));
+    }
+
+    public function testSanitizeSvgDocumentAllowsSafeImageDataUriAndBlocksUnsafeDataUri(): void
+    {
+        $safe = '<svg xmlns="http://www.w3.org/2000/svg"><image href="data:image/png;base64,QUJDRA=="/></svg>';
+        self::assertSame($safe, sanitize_svg_document($safe));
+
+        $unsafe = '<svg xmlns="http://www.w3.org/2000/svg"><image href="data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=="/></svg>';
+        $sanitizedUnsafe = sanitize_svg_document($unsafe);
+        self::assertStringContainsString('QSL sécurisée indisponible', $sanitizedUnsafe);
     }
 
     public function testGenerateQslSvgEscapesDynamicPayloadValues(): void
