082376ed8abf47d5c1a28ef88eabf1fb393b28f4
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2026-05-21 21:13:03 +0200

    refactor(i18n): modularize admin_permissions into 14-locale files

---
+++ app/i18n/admin_permissions/de.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'role_assigned' => 'Rolle zugewiesen.',
+    'role_removed' => 'Rolle entfernt.',
+    'title' => 'Rollen und Berechtigungen',
+    'th_permission' => 'Berechtigung',
+    'th_label' => 'Bezeichnung',
+    'assign_role' => 'Rolle zuweisen',
+    'member' => 'Mitglied',
+    'role' => 'Rolle',
+    'assign' => 'Zuweisen',
+    'layout' => 'Rollen & Berechtigungen',
+    'meta_desc' => 'Rollen und Berechtigungen verwalten.',
+    'remove_role' => 'Entfernen',
+    'assignments' => 'Zuweisungen',
+];
---
+++ app/i18n/admin_permissions/ru.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'role_assigned' => 'Роль назначена.',
+    'role_removed' => 'Роль удалена.',
+    'title' => 'Роли и права',
+    'th_permission' => 'Право',
+    'th_label' => 'Метка',
+    'assign_role' => 'Назначить роль',
+    'member' => 'Участник',
+    'role' => 'Роль',
+    'assign' => 'Назначить',
+    'layout' => 'Роли и права',
+    'meta_desc' => 'Управление ролями и правами.',
+    'remove_role' => 'Удалить',
+    'assignments' => 'Назначения',
+];
