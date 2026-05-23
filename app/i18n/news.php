63869306557aa3f4e8857f9067517f5dbb13a2ca
odebauche <odebauche@awegroupe.be>
2026-05-23 21:11:59 +0200

    improvement phase II

---
+++ app/i18n/news.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'fr' => require __DIR__ . '/news/fr.php',
+    'en' => require __DIR__ . '/news/en.php',
+    'de' => require __DIR__ . '/news/de.php',
+    'nl' => require __DIR__ . '/news/nl.php',
+    'it' => require __DIR__ . '/news/it.php',
+    'es' => require __DIR__ . '/news/es.php',
+    'pt' => require __DIR__ . '/news/pt.php',
+    'ar' => require __DIR__ . '/news/ar.php',
+    'hi' => require __DIR__ . '/news/hi.php',
+    'ja' => require __DIR__ . '/news/ja.php',
+    'zh' => require __DIR__ . '/news/zh.php',
+    'bn' => require __DIR__ . '/news/bn.php',
+    'ru' => require __DIR__ . '/news/ru.php',
+    'id' => require __DIR__ . '/news/id.php',
+];
