36c7cd5a1f09a62e4b93599c704b1764691bcbc6
odebauche <odebauche@awegroupe.be>
2026-05-23 13:01:41 +0200

    improvement phase II

---
+++ app/i18n/code_q.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'fr' => require __DIR__ . '/code_q/fr.php',
+    'en' => require __DIR__ . '/code_q/en.php',
+];
---
+++ app/i18n/settings.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'fr' => require __DIR__ . '/settings/fr.php',
+    'en' => require __DIR__ . '/settings/en.php',
+];
