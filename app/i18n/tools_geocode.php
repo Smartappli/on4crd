3fa7eb5014019be457cd4abd1330c53ac8dd114c
odebauche <odebauche@awegroupe.be>
2026-05-31 08:28:11 +0200

    improvement phase IV

--- app/i18n/tools_geocode.php
+++ app/i18n/tools_geocode.php
@@ -1,17 +1,37 @@
 <?php
-return [
-    'fr' => require __DIR__ . '/tools_geocode/fr.php',
-    'en' => require __DIR__ . '/tools_geocode/en.php',
-    'de' => require __DIR__ . '/tools_geocode/de.php',
-    'nl' => require __DIR__ . '/tools_geocode/nl.php',
-    'es' => require __DIR__ . '/tools_geocode/es.php',
-    'it' => require __DIR__ . '/tools_geocode/it.php',
-    'pt' => require __DIR__ . '/tools_geocode/pt.php',
-    'ar' => require __DIR__ . '/tools_geocode/ar.php',
-    'hi' => require __DIR__ . '/tools_geocode/hi.php',
-    'ja' => require __DIR__ . '/tools_geocode/ja.php',
-    'zh' => require __DIR__ . '/tools_geocode/zh.php',
-    'bn' => require __DIR__ . '/tools_geocode/bn.php',
-    'ru' => require __DIR__ . '/tools_geocode/ru.php',
-    'id' => require __DIR__ . '/tools_geocode/id.php',
-];
+declare(strict_types=1);
+
+$messages = [];
+$messages['fr'] = require __DIR__ . '/tools_geocode/fr.php';
+$messages['en'] = require __DIR__ . '/tools_geocode/en.php';
+$messages['de'] = require __DIR__ . '/tools_geocode/de.php';
+$messages['nl'] = require __DIR__ . '/tools_geocode/nl.php';
+$messages['it'] = require __DIR__ . '/tools_geocode/it.php';
+$messages['es'] = require __DIR__ . '/tools_geocode/es.php';
+$messages['pt'] = require __DIR__ . '/tools_geocode/pt.php';
+$messages['bg'] = require __DIR__ . '/tools_geocode/bg.php';
+$messages['hr'] = require __DIR__ . '/tools_geocode/hr.php';
+$messages['cs'] = require __DIR__ . '/tools_geocode/cs.php';
+$messages['da'] = require __DIR__ . '/tools_geocode/da.php';
+$messages['et'] = require __DIR__ . '/tools_geocode/et.php';
+$messages['fi'] = require __DIR__ . '/tools_geocode/fi.php';
+$messages['el'] = require __DIR__ . '/tools_geocode/el.php';
+$messages['hu'] = require __DIR__ . '/tools_geocode/hu.php';
+$messages['ga'] = require __DIR__ . '/tools_geocode/ga.php';
+$messages['lv'] = require __DIR__ . '/tools_geocode/lv.php';
+$messages['lt'] = require __DIR__ . '/tools_geocode/lt.php';
+$messages['mt'] = require __DIR__ . '/tools_geocode/mt.php';
+$messages['pl'] = require __DIR__ . '/tools_geocode/pl.php';
+$messages['ro'] = require __DIR__ . '/tools_geocode/ro.php';
+$messages['sk'] = require __DIR__ . '/tools_geocode/sk.php';
+$messages['sl'] = require __DIR__ . '/tools_geocode/sl.php';
+$messages['sv'] = require __DIR__ . '/tools_geocode/sv.php';
+$messages['ar'] = require __DIR__ . '/tools_geocode/ar.php';
+$messages['hi'] = require __DIR__ . '/tools_geocode/hi.php';
+$messages['ja'] = require __DIR__ . '/tools_geocode/ja.php';
+$messages['zh'] = require __DIR__ . '/tools_geocode/zh.php';
+$messages['bn'] = require __DIR__ . '/tools_geocode/bn.php';
+$messages['ru'] = require __DIR__ . '/tools_geocode/ru.php';
+$messages['id'] = require __DIR__ . '/tools_geocode/id.php';
+
+return $messages;
