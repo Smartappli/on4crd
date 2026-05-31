2626ae7666108894d1fe83fc6c73024487b5b37d
odebauche <odebauche@awegroupe.be>
2026-05-31 07:56:46 +0200

    improvement phase IV

--- app/i18n/dashboard.php
+++ app/i18n/dashboard.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/dashboard/fr.php',
-    'en' => require __DIR__ . '/dashboard/en.php',
-    'de' => require __DIR__ . '/dashboard/de.php',
-    'nl' => require __DIR__ . '/dashboard/nl.php',
-    'it' => require __DIR__ . '/dashboard/it.php',
-    'es' => require __DIR__ . '/dashboard/es.php',
-    'pt' => require __DIR__ . '/dashboard/pt.php',
-    'ar' => require __DIR__ . '/dashboard/ar.php',
-    'hi' => require __DIR__ . '/dashboard/hi.php',
-    'ja' => require __DIR__ . '/dashboard/ja.php',
-    'zh' => require __DIR__ . '/dashboard/zh.php',
-    'bn' => require __DIR__ . '/dashboard/bn.php',
-    'ru' => require __DIR__ . '/dashboard/ru.php',
-    'id' => require __DIR__ . '/dashboard/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/dashboard/fr.php';
+$messages['en'] = require __DIR__ . '/dashboard/en.php';
+$messages['de'] = require __DIR__ . '/dashboard/de.php';
+$messages['nl'] = require __DIR__ . '/dashboard/nl.php';
+$messages['it'] = require __DIR__ . '/dashboard/it.php';
+$messages['es'] = require __DIR__ . '/dashboard/es.php';
+$messages['pt'] = require __DIR__ . '/dashboard/pt.php';
+$messages['bg'] = require __DIR__ . '/dashboard/bg.php';
+$messages['hr'] = require __DIR__ . '/dashboard/hr.php';
+$messages['cs'] = require __DIR__ . '/dashboard/cs.php';
+$messages['da'] = require __DIR__ . '/dashboard/da.php';
+$messages['et'] = require __DIR__ . '/dashboard/et.php';
+$messages['fi'] = require __DIR__ . '/dashboard/fi.php';
+$messages['el'] = require __DIR__ . '/dashboard/el.php';
+$messages['hu'] = require __DIR__ . '/dashboard/hu.php';
+$messages['ga'] = require __DIR__ . '/dashboard/ga.php';
+$messages['lv'] = require __DIR__ . '/dashboard/lv.php';
+$messages['lt'] = require __DIR__ . '/dashboard/lt.php';
+$messages['mt'] = require __DIR__ . '/dashboard/mt.php';
+$messages['pl'] = require __DIR__ . '/dashboard/pl.php';
+$messages['ro'] = require __DIR__ . '/dashboard/ro.php';
+$messages['sk'] = require __DIR__ . '/dashboard/sk.php';
+$messages['sl'] = require __DIR__ . '/dashboard/sl.php';
+$messages['sv'] = require __DIR__ . '/dashboard/sv.php';
+$messages['ar'] = require __DIR__ . '/dashboard/ar.php';
+$messages['hi'] = require __DIR__ . '/dashboard/hi.php';
+$messages['ja'] = require __DIR__ . '/dashboard/ja.php';
+$messages['zh'] = require __DIR__ . '/dashboard/zh.php';
+$messages['bn'] = require __DIR__ . '/dashboard/bn.php';
+$messages['ru'] = require __DIR__ . '/dashboard/ru.php';
+$messages['id'] = require __DIR__ . '/dashboard/id.php';
+
+return $messages;
