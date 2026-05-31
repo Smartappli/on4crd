2ecc6ace6f55df968720f8aa71121cd0bc071e95
odebauche <odebauche@awegroupe.be>
2026-05-31 07:51:40 +0200

    improvement phase IV

--- app/i18n/chatbot.php
+++ app/i18n/chatbot.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/chatbot/fr.php',
-    'en' => require __DIR__ . '/chatbot/en.php',
-    'de' => require __DIR__ . '/chatbot/de.php',
-    'nl' => require __DIR__ . '/chatbot/nl.php',
-    'it' => require __DIR__ . '/chatbot/it.php',
-    'es' => require __DIR__ . '/chatbot/es.php',
-    'pt' => require __DIR__ . '/chatbot/pt.php',
-    'ar' => require __DIR__ . '/chatbot/ar.php',
-    'hi' => require __DIR__ . '/chatbot/hi.php',
-    'ja' => require __DIR__ . '/chatbot/ja.php',
-    'zh' => require __DIR__ . '/chatbot/zh.php',
-    'bn' => require __DIR__ . '/chatbot/bn.php',
-    'ru' => require __DIR__ . '/chatbot/ru.php',
-    'id' => require __DIR__ . '/chatbot/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/chatbot/fr.php';
+$messages['en'] = require __DIR__ . '/chatbot/en.php';
+$messages['de'] = require __DIR__ . '/chatbot/de.php';
+$messages['nl'] = require __DIR__ . '/chatbot/nl.php';
+$messages['it'] = require __DIR__ . '/chatbot/it.php';
+$messages['es'] = require __DIR__ . '/chatbot/es.php';
+$messages['pt'] = require __DIR__ . '/chatbot/pt.php';
+$messages['bg'] = require __DIR__ . '/chatbot/bg.php';
+$messages['hr'] = require __DIR__ . '/chatbot/hr.php';
+$messages['cs'] = require __DIR__ . '/chatbot/cs.php';
+$messages['da'] = require __DIR__ . '/chatbot/da.php';
+$messages['et'] = require __DIR__ . '/chatbot/et.php';
+$messages['fi'] = require __DIR__ . '/chatbot/fi.php';
+$messages['el'] = require __DIR__ . '/chatbot/el.php';
+$messages['hu'] = require __DIR__ . '/chatbot/hu.php';
+$messages['ga'] = require __DIR__ . '/chatbot/ga.php';
+$messages['lv'] = require __DIR__ . '/chatbot/lv.php';
+$messages['lt'] = require __DIR__ . '/chatbot/lt.php';
+$messages['mt'] = require __DIR__ . '/chatbot/mt.php';
+$messages['pl'] = require __DIR__ . '/chatbot/pl.php';
+$messages['ro'] = require __DIR__ . '/chatbot/ro.php';
+$messages['sk'] = require __DIR__ . '/chatbot/sk.php';
+$messages['sl'] = require __DIR__ . '/chatbot/sl.php';
+$messages['sv'] = require __DIR__ . '/chatbot/sv.php';
+$messages['ar'] = require __DIR__ . '/chatbot/ar.php';
+$messages['hi'] = require __DIR__ . '/chatbot/hi.php';
+$messages['ja'] = require __DIR__ . '/chatbot/ja.php';
+$messages['zh'] = require __DIR__ . '/chatbot/zh.php';
+$messages['bn'] = require __DIR__ . '/chatbot/bn.php';
+$messages['ru'] = require __DIR__ . '/chatbot/ru.php';
+$messages['id'] = require __DIR__ . '/chatbot/id.php';
+
+return $messages;
