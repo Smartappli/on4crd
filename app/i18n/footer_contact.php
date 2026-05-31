fab5ec339454d04a22b253dc3154a19ea437cb27
odebauche <odebauche@awegroupe.be>
2026-05-31 08:00:04 +0200

    improvement phase IV

--- app/i18n/footer_contact.php
+++ app/i18n/footer_contact.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/footer_contact/fr.php',
-    'en' => require __DIR__ . '/footer_contact/en.php',
-    'de' => require __DIR__ . '/footer_contact/de.php',
-    'nl' => require __DIR__ . '/footer_contact/nl.php',
-    'it' => require __DIR__ . '/footer_contact/it.php',
-    'es' => require __DIR__ . '/footer_contact/es.php',
-    'pt' => require __DIR__ . '/footer_contact/pt.php',
-    'ar' => require __DIR__ . '/footer_contact/ar.php',
-    'hi' => require __DIR__ . '/footer_contact/hi.php',
-    'ja' => require __DIR__ . '/footer_contact/ja.php',
-    'zh' => require __DIR__ . '/footer_contact/zh.php',
-    'bn' => require __DIR__ . '/footer_contact/bn.php',
-    'ru' => require __DIR__ . '/footer_contact/ru.php',
-    'id' => require __DIR__ . '/footer_contact/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/footer_contact/fr.php';
+$messages['en'] = require __DIR__ . '/footer_contact/en.php';
+$messages['de'] = require __DIR__ . '/footer_contact/de.php';
+$messages['nl'] = require __DIR__ . '/footer_contact/nl.php';
+$messages['it'] = require __DIR__ . '/footer_contact/it.php';
+$messages['es'] = require __DIR__ . '/footer_contact/es.php';
+$messages['pt'] = require __DIR__ . '/footer_contact/pt.php';
+$messages['bg'] = require __DIR__ . '/footer_contact/bg.php';
+$messages['hr'] = require __DIR__ . '/footer_contact/hr.php';
+$messages['cs'] = require __DIR__ . '/footer_contact/cs.php';
+$messages['da'] = require __DIR__ . '/footer_contact/da.php';
+$messages['et'] = require __DIR__ . '/footer_contact/et.php';
+$messages['fi'] = require __DIR__ . '/footer_contact/fi.php';
+$messages['el'] = require __DIR__ . '/footer_contact/el.php';
+$messages['hu'] = require __DIR__ . '/footer_contact/hu.php';
+$messages['ga'] = require __DIR__ . '/footer_contact/ga.php';
+$messages['lv'] = require __DIR__ . '/footer_contact/lv.php';
+$messages['lt'] = require __DIR__ . '/footer_contact/lt.php';
+$messages['mt'] = require __DIR__ . '/footer_contact/mt.php';
+$messages['pl'] = require __DIR__ . '/footer_contact/pl.php';
+$messages['ro'] = require __DIR__ . '/footer_contact/ro.php';
+$messages['sk'] = require __DIR__ . '/footer_contact/sk.php';
+$messages['sl'] = require __DIR__ . '/footer_contact/sl.php';
+$messages['sv'] = require __DIR__ . '/footer_contact/sv.php';
+$messages['ar'] = require __DIR__ . '/footer_contact/ar.php';
+$messages['hi'] = require __DIR__ . '/footer_contact/hi.php';
+$messages['ja'] = require __DIR__ . '/footer_contact/ja.php';
+$messages['zh'] = require __DIR__ . '/footer_contact/zh.php';
+$messages['bn'] = require __DIR__ . '/footer_contact/bn.php';
+$messages['ru'] = require __DIR__ . '/footer_contact/ru.php';
+$messages['id'] = require __DIR__ . '/footer_contact/id.php';
+
+return $messages;
