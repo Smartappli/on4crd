2c5fe9501813dbaea14541bc6358a15189af9c78
odebauche <odebauche@awegroupe.be>
2026-05-31 08:29:03 +0200

    improvement phase IV

--- app/i18n/wiki_edit.php
+++ app/i18n/wiki_edit.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/wiki_edit/fr.php',
-    'en' => require __DIR__ . '/wiki_edit/en.php',
-    'de' => require __DIR__ . '/wiki_edit/de.php',
-    'nl' => require __DIR__ . '/wiki_edit/nl.php',
-    'it' => require __DIR__ . '/wiki_edit/it.php',
-    'es' => require __DIR__ . '/wiki_edit/es.php',
-    'pt' => require __DIR__ . '/wiki_edit/pt.php',
-    'ar' => require __DIR__ . '/wiki_edit/ar.php',
-    'hi' => require __DIR__ . '/wiki_edit/hi.php',
-    'ja' => require __DIR__ . '/wiki_edit/ja.php',
-    'zh' => require __DIR__ . '/wiki_edit/zh.php',
-    'bn' => require __DIR__ . '/wiki_edit/bn.php',
-    'ru' => require __DIR__ . '/wiki_edit/ru.php',
-    'id' => require __DIR__ . '/wiki_edit/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/wiki_edit/fr.php';
+$messages['en'] = require __DIR__ . '/wiki_edit/en.php';
+$messages['de'] = require __DIR__ . '/wiki_edit/de.php';
+$messages['nl'] = require __DIR__ . '/wiki_edit/nl.php';
+$messages['it'] = require __DIR__ . '/wiki_edit/it.php';
+$messages['es'] = require __DIR__ . '/wiki_edit/es.php';
+$messages['pt'] = require __DIR__ . '/wiki_edit/pt.php';
+$messages['bg'] = require __DIR__ . '/wiki_edit/bg.php';
+$messages['hr'] = require __DIR__ . '/wiki_edit/hr.php';
+$messages['cs'] = require __DIR__ . '/wiki_edit/cs.php';
+$messages['da'] = require __DIR__ . '/wiki_edit/da.php';
+$messages['et'] = require __DIR__ . '/wiki_edit/et.php';
+$messages['fi'] = require __DIR__ . '/wiki_edit/fi.php';
+$messages['el'] = require __DIR__ . '/wiki_edit/el.php';
+$messages['hu'] = require __DIR__ . '/wiki_edit/hu.php';
+$messages['ga'] = require __DIR__ . '/wiki_edit/ga.php';
+$messages['lv'] = require __DIR__ . '/wiki_edit/lv.php';
+$messages['lt'] = require __DIR__ . '/wiki_edit/lt.php';
+$messages['mt'] = require __DIR__ . '/wiki_edit/mt.php';
+$messages['pl'] = require __DIR__ . '/wiki_edit/pl.php';
+$messages['ro'] = require __DIR__ . '/wiki_edit/ro.php';
+$messages['sk'] = require __DIR__ . '/wiki_edit/sk.php';
+$messages['sl'] = require __DIR__ . '/wiki_edit/sl.php';
+$messages['sv'] = require __DIR__ . '/wiki_edit/sv.php';
+$messages['ar'] = require __DIR__ . '/wiki_edit/ar.php';
+$messages['hi'] = require __DIR__ . '/wiki_edit/hi.php';
+$messages['ja'] = require __DIR__ . '/wiki_edit/ja.php';
+$messages['zh'] = require __DIR__ . '/wiki_edit/zh.php';
+$messages['bn'] = require __DIR__ . '/wiki_edit/bn.php';
+$messages['ru'] = require __DIR__ . '/wiki_edit/ru.php';
+$messages['id'] = require __DIR__ . '/wiki_edit/id.php';
+
+return $messages;
