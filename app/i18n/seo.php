1e9ee9674d4b345328bd1dfad1107182ac6849b2
odebauche <odebauche@awegroupe.be>
2026-05-31 08:23:31 +0200

    improvement phase IV

--- app/i18n/seo.php
+++ app/i18n/seo.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/seo/fr.php',
-    'en' => require __DIR__ . '/seo/en.php',
-    'de' => require __DIR__ . '/seo/de.php',
-    'nl' => require __DIR__ . '/seo/nl.php',
-    'it' => require __DIR__ . '/seo/it.php',
-    'es' => require __DIR__ . '/seo/es.php',
-    'pt' => require __DIR__ . '/seo/pt.php',
-    'ar' => require __DIR__ . '/seo/ar.php',
-    'hi' => require __DIR__ . '/seo/hi.php',
-    'ja' => require __DIR__ . '/seo/ja.php',
-    'zh' => require __DIR__ . '/seo/zh.php',
-    'bn' => require __DIR__ . '/seo/bn.php',
-    'ru' => require __DIR__ . '/seo/ru.php',
-    'id' => require __DIR__ . '/seo/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/seo/fr.php';
+$messages['en'] = require __DIR__ . '/seo/en.php';
+$messages['de'] = require __DIR__ . '/seo/de.php';
+$messages['nl'] = require __DIR__ . '/seo/nl.php';
+$messages['it'] = require __DIR__ . '/seo/it.php';
+$messages['es'] = require __DIR__ . '/seo/es.php';
+$messages['pt'] = require __DIR__ . '/seo/pt.php';
+$messages['bg'] = require __DIR__ . '/seo/bg.php';
+$messages['hr'] = require __DIR__ . '/seo/hr.php';
+$messages['cs'] = require __DIR__ . '/seo/cs.php';
+$messages['da'] = require __DIR__ . '/seo/da.php';
+$messages['et'] = require __DIR__ . '/seo/et.php';
+$messages['fi'] = require __DIR__ . '/seo/fi.php';
+$messages['el'] = require __DIR__ . '/seo/el.php';
+$messages['hu'] = require __DIR__ . '/seo/hu.php';
+$messages['ga'] = require __DIR__ . '/seo/ga.php';
+$messages['lv'] = require __DIR__ . '/seo/lv.php';
+$messages['lt'] = require __DIR__ . '/seo/lt.php';
+$messages['mt'] = require __DIR__ . '/seo/mt.php';
+$messages['pl'] = require __DIR__ . '/seo/pl.php';
+$messages['ro'] = require __DIR__ . '/seo/ro.php';
+$messages['sk'] = require __DIR__ . '/seo/sk.php';
+$messages['sl'] = require __DIR__ . '/seo/sl.php';
+$messages['sv'] = require __DIR__ . '/seo/sv.php';
+$messages['ar'] = require __DIR__ . '/seo/ar.php';
+$messages['hi'] = require __DIR__ . '/seo/hi.php';
+$messages['ja'] = require __DIR__ . '/seo/ja.php';
+$messages['zh'] = require __DIR__ . '/seo/zh.php';
+$messages['bn'] = require __DIR__ . '/seo/bn.php';
+$messages['ru'] = require __DIR__ . '/seo/ru.php';
+$messages['id'] = require __DIR__ . '/seo/id.php';
+
+return $messages;
