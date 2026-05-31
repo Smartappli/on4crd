608f510e7d39bfa0ddd094eba9cef55d73cf63f7
odebauche <odebauche@awegroupe.be>
2026-05-31 07:45:17 +0200

    improvement phase IV

--- app/i18n/admin_wiki.php
+++ app/i18n/admin_wiki.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/admin_wiki/fr.php',
-    'en' => require __DIR__ . '/admin_wiki/en.php',
-    'de' => require __DIR__ . '/admin_wiki/de.php',
-    'nl' => require __DIR__ . '/admin_wiki/nl.php',
-    'es' => require __DIR__ . '/admin_wiki/es.php',
-    'it' => require __DIR__ . '/admin_wiki/it.php',
-    'pt' => require __DIR__ . '/admin_wiki/pt.php',
-    'ar' => require __DIR__ . '/admin_wiki/ar.php',
-    'hi' => require __DIR__ . '/admin_wiki/hi.php',
-    'ja' => require __DIR__ . '/admin_wiki/ja.php',
-    'zh' => require __DIR__ . '/admin_wiki/zh.php',
-    'bn' => require __DIR__ . '/admin_wiki/bn.php',
-    'ru' => require __DIR__ . '/admin_wiki/ru.php',
-    'id' => require __DIR__ . '/admin_wiki/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/admin_wiki/fr.php';
+$messages['en'] = require __DIR__ . '/admin_wiki/en.php';
+$messages['de'] = require __DIR__ . '/admin_wiki/de.php';
+$messages['nl'] = require __DIR__ . '/admin_wiki/nl.php';
+$messages['it'] = require __DIR__ . '/admin_wiki/it.php';
+$messages['es'] = require __DIR__ . '/admin_wiki/es.php';
+$messages['pt'] = require __DIR__ . '/admin_wiki/pt.php';
+$messages['bg'] = require __DIR__ . '/admin_wiki/bg.php';
+$messages['hr'] = require __DIR__ . '/admin_wiki/hr.php';
+$messages['cs'] = require __DIR__ . '/admin_wiki/cs.php';
+$messages['da'] = require __DIR__ . '/admin_wiki/da.php';
+$messages['et'] = require __DIR__ . '/admin_wiki/et.php';
+$messages['fi'] = require __DIR__ . '/admin_wiki/fi.php';
+$messages['el'] = require __DIR__ . '/admin_wiki/el.php';
+$messages['hu'] = require __DIR__ . '/admin_wiki/hu.php';
+$messages['ga'] = require __DIR__ . '/admin_wiki/ga.php';
+$messages['lv'] = require __DIR__ . '/admin_wiki/lv.php';
+$messages['lt'] = require __DIR__ . '/admin_wiki/lt.php';
+$messages['mt'] = require __DIR__ . '/admin_wiki/mt.php';
+$messages['pl'] = require __DIR__ . '/admin_wiki/pl.php';
+$messages['ro'] = require __DIR__ . '/admin_wiki/ro.php';
+$messages['sk'] = require __DIR__ . '/admin_wiki/sk.php';
+$messages['sl'] = require __DIR__ . '/admin_wiki/sl.php';
+$messages['sv'] = require __DIR__ . '/admin_wiki/sv.php';
+$messages['ar'] = require __DIR__ . '/admin_wiki/ar.php';
+$messages['hi'] = require __DIR__ . '/admin_wiki/hi.php';
+$messages['ja'] = require __DIR__ . '/admin_wiki/ja.php';
+$messages['zh'] = require __DIR__ . '/admin_wiki/zh.php';
+$messages['bn'] = require __DIR__ . '/admin_wiki/bn.php';
+$messages['ru'] = require __DIR__ . '/admin_wiki/ru.php';
+$messages['id'] = require __DIR__ . '/admin_wiki/id.php';
+
+return $messages;
