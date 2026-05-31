f4dbc4bd9ea8d0d3d6418597831bf1ca2b04c67a
odebauche <odebauche@awegroupe.be>
2026-05-31 08:08:25 +0200

    improvement phase IV

--- app/i18n/news.php
+++ app/i18n/news.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/news/fr.php',
-    'en' => require __DIR__ . '/news/en.php',
-    'de' => require __DIR__ . '/news/de.php',
-    'nl' => require __DIR__ . '/news/nl.php',
-    'it' => require __DIR__ . '/news/it.php',
-    'es' => require __DIR__ . '/news/es.php',
-    'pt' => require __DIR__ . '/news/pt.php',
-    'ar' => require __DIR__ . '/news/ar.php',
-    'hi' => require __DIR__ . '/news/hi.php',
-    'ja' => require __DIR__ . '/news/ja.php',
-    'zh' => require __DIR__ . '/news/zh.php',
-    'bn' => require __DIR__ . '/news/bn.php',
-    'ru' => require __DIR__ . '/news/ru.php',
-    'id' => require __DIR__ . '/news/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/news/fr.php';
+$messages['en'] = require __DIR__ . '/news/en.php';
+$messages['de'] = require __DIR__ . '/news/de.php';
+$messages['nl'] = require __DIR__ . '/news/nl.php';
+$messages['it'] = require __DIR__ . '/news/it.php';
+$messages['es'] = require __DIR__ . '/news/es.php';
+$messages['pt'] = require __DIR__ . '/news/pt.php';
+$messages['bg'] = require __DIR__ . '/news/bg.php';
+$messages['hr'] = require __DIR__ . '/news/hr.php';
+$messages['cs'] = require __DIR__ . '/news/cs.php';
+$messages['da'] = require __DIR__ . '/news/da.php';
+$messages['et'] = require __DIR__ . '/news/et.php';
+$messages['fi'] = require __DIR__ . '/news/fi.php';
+$messages['el'] = require __DIR__ . '/news/el.php';
+$messages['hu'] = require __DIR__ . '/news/hu.php';
+$messages['ga'] = require __DIR__ . '/news/ga.php';
+$messages['lv'] = require __DIR__ . '/news/lv.php';
+$messages['lt'] = require __DIR__ . '/news/lt.php';
+$messages['mt'] = require __DIR__ . '/news/mt.php';
+$messages['pl'] = require __DIR__ . '/news/pl.php';
+$messages['ro'] = require __DIR__ . '/news/ro.php';
+$messages['sk'] = require __DIR__ . '/news/sk.php';
+$messages['sl'] = require __DIR__ . '/news/sl.php';
+$messages['sv'] = require __DIR__ . '/news/sv.php';
+$messages['ar'] = require __DIR__ . '/news/ar.php';
+$messages['hi'] = require __DIR__ . '/news/hi.php';
+$messages['ja'] = require __DIR__ . '/news/ja.php';
+$messages['zh'] = require __DIR__ . '/news/zh.php';
+$messages['bn'] = require __DIR__ . '/news/bn.php';
+$messages['ru'] = require __DIR__ . '/news/ru.php';
+$messages['id'] = require __DIR__ . '/news/id.php';
+
+return $messages;
