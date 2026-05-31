033053f4288ee5e628dfb5b647a10d1fd81d4186
odebauche <odebauche@awegroupe.be>
2026-05-31 08:05:55 +0200

    improvement phase IV

--- app/i18n/login.php
+++ app/i18n/login.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/login/fr.php',
-    'en' => require __DIR__ . '/login/en.php',
-    'de' => require __DIR__ . '/login/de.php',
-    'es' => require __DIR__ . '/login/es.php',
-    'it' => require __DIR__ . '/login/it.php',
-    'pt' => require __DIR__ . '/login/pt.php',
-    'nl' => require __DIR__ . '/login/nl.php',
-    'ar' => require __DIR__ . '/login/ar.php',
-    'hi' => require __DIR__ . '/login/hi.php',
-    'ja' => require __DIR__ . '/login/ja.php',
-    'zh' => require __DIR__ . '/login/zh.php',
-    'bn' => require __DIR__ . '/login/bn.php',
-    'ru' => require __DIR__ . '/login/ru.php',
-    'id' => require __DIR__ . '/login/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/login/fr.php';
+$messages['en'] = require __DIR__ . '/login/en.php';
+$messages['de'] = require __DIR__ . '/login/de.php';
+$messages['nl'] = require __DIR__ . '/login/nl.php';
+$messages['it'] = require __DIR__ . '/login/it.php';
+$messages['es'] = require __DIR__ . '/login/es.php';
+$messages['pt'] = require __DIR__ . '/login/pt.php';
+$messages['bg'] = require __DIR__ . '/login/bg.php';
+$messages['hr'] = require __DIR__ . '/login/hr.php';
+$messages['cs'] = require __DIR__ . '/login/cs.php';
+$messages['da'] = require __DIR__ . '/login/da.php';
+$messages['et'] = require __DIR__ . '/login/et.php';
+$messages['fi'] = require __DIR__ . '/login/fi.php';
+$messages['el'] = require __DIR__ . '/login/el.php';
+$messages['hu'] = require __DIR__ . '/login/hu.php';
+$messages['ga'] = require __DIR__ . '/login/ga.php';
+$messages['lv'] = require __DIR__ . '/login/lv.php';
+$messages['lt'] = require __DIR__ . '/login/lt.php';
+$messages['mt'] = require __DIR__ . '/login/mt.php';
+$messages['pl'] = require __DIR__ . '/login/pl.php';
+$messages['ro'] = require __DIR__ . '/login/ro.php';
+$messages['sk'] = require __DIR__ . '/login/sk.php';
+$messages['sl'] = require __DIR__ . '/login/sl.php';
+$messages['sv'] = require __DIR__ . '/login/sv.php';
+$messages['ar'] = require __DIR__ . '/login/ar.php';
+$messages['hi'] = require __DIR__ . '/login/hi.php';
+$messages['ja'] = require __DIR__ . '/login/ja.php';
+$messages['zh'] = require __DIR__ . '/login/zh.php';
+$messages['bn'] = require __DIR__ . '/login/bn.php';
+$messages['ru'] = require __DIR__ . '/login/ru.php';
+$messages['id'] = require __DIR__ . '/login/id.php';
+
+return $messages;
