9b557d812781f3bcc4874160f6e39e187ae322e8
odebauche <odebauche@awegroupe.be>
2026-05-31 07:54:08 +0200

    improvement phase IV

--- app/i18n/code_q.php
+++ app/i18n/code_q.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/code_q/fr.php',
-    'en' => require __DIR__ . '/code_q/en.php',
-    'de' => require __DIR__ . '/code_q/de.php',
-    'nl' => require __DIR__ . '/code_q/nl.php',
-    'it' => require __DIR__ . '/code_q/it.php',
-    'es' => require __DIR__ . '/code_q/es.php',
-    'pt' => require __DIR__ . '/code_q/pt.php',
-    'ar' => require __DIR__ . '/code_q/ar.php',
-    'hi' => require __DIR__ . '/code_q/hi.php',
-    'ja' => require __DIR__ . '/code_q/ja.php',
-    'zh' => require __DIR__ . '/code_q/zh.php',
-    'bn' => require __DIR__ . '/code_q/bn.php',
-    'ru' => require __DIR__ . '/code_q/ru.php',
-    'id' => require __DIR__ . '/code_q/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/code_q/fr.php';
+$messages['en'] = require __DIR__ . '/code_q/en.php';
+$messages['de'] = require __DIR__ . '/code_q/de.php';
+$messages['nl'] = require __DIR__ . '/code_q/nl.php';
+$messages['it'] = require __DIR__ . '/code_q/it.php';
+$messages['es'] = require __DIR__ . '/code_q/es.php';
+$messages['pt'] = require __DIR__ . '/code_q/pt.php';
+$messages['bg'] = require __DIR__ . '/code_q/bg.php';
+$messages['hr'] = require __DIR__ . '/code_q/hr.php';
+$messages['cs'] = require __DIR__ . '/code_q/cs.php';
+$messages['da'] = require __DIR__ . '/code_q/da.php';
+$messages['et'] = require __DIR__ . '/code_q/et.php';
+$messages['fi'] = require __DIR__ . '/code_q/fi.php';
+$messages['el'] = require __DIR__ . '/code_q/el.php';
+$messages['hu'] = require __DIR__ . '/code_q/hu.php';
+$messages['ga'] = require __DIR__ . '/code_q/ga.php';
+$messages['lv'] = require __DIR__ . '/code_q/lv.php';
+$messages['lt'] = require __DIR__ . '/code_q/lt.php';
+$messages['mt'] = require __DIR__ . '/code_q/mt.php';
+$messages['pl'] = require __DIR__ . '/code_q/pl.php';
+$messages['ro'] = require __DIR__ . '/code_q/ro.php';
+$messages['sk'] = require __DIR__ . '/code_q/sk.php';
+$messages['sl'] = require __DIR__ . '/code_q/sl.php';
+$messages['sv'] = require __DIR__ . '/code_q/sv.php';
+$messages['ar'] = require __DIR__ . '/code_q/ar.php';
+$messages['hi'] = require __DIR__ . '/code_q/hi.php';
+$messages['ja'] = require __DIR__ . '/code_q/ja.php';
+$messages['zh'] = require __DIR__ . '/code_q/zh.php';
+$messages['bn'] = require __DIR__ . '/code_q/bn.php';
+$messages['ru'] = require __DIR__ . '/code_q/ru.php';
+$messages['id'] = require __DIR__ . '/code_q/id.php';
+
+return $messages;
