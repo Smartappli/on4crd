1c5aaf78f81a15ab3f67d7ca086c5088342112e1
odebauche <odebauche@awegroupe.be>
2026-05-31 07:56:08 +0200

    improvement phase IV

--- app/i18n/conditions_utilisation.php
+++ app/i18n/conditions_utilisation.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/conditions_utilisation/fr.php',
-    'en' => require __DIR__ . '/conditions_utilisation/en.php',
-    'de' => require __DIR__ . '/conditions_utilisation/de.php',
-    'nl' => require __DIR__ . '/conditions_utilisation/nl.php',
-    'it' => require __DIR__ . '/conditions_utilisation/it.php',
-    'es' => require __DIR__ . '/conditions_utilisation/es.php',
-    'pt' => require __DIR__ . '/conditions_utilisation/pt.php',
-    'ar' => require __DIR__ . '/conditions_utilisation/ar.php',
-    'hi' => require __DIR__ . '/conditions_utilisation/hi.php',
-    'ja' => require __DIR__ . '/conditions_utilisation/ja.php',
-    'zh' => require __DIR__ . '/conditions_utilisation/zh.php',
-    'bn' => require __DIR__ . '/conditions_utilisation/bn.php',
-    'ru' => require __DIR__ . '/conditions_utilisation/ru.php',
-    'id' => require __DIR__ . '/conditions_utilisation/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/conditions_utilisation/fr.php';
+$messages['en'] = require __DIR__ . '/conditions_utilisation/en.php';
+$messages['de'] = require __DIR__ . '/conditions_utilisation/de.php';
+$messages['nl'] = require __DIR__ . '/conditions_utilisation/nl.php';
+$messages['it'] = require __DIR__ . '/conditions_utilisation/it.php';
+$messages['es'] = require __DIR__ . '/conditions_utilisation/es.php';
+$messages['pt'] = require __DIR__ . '/conditions_utilisation/pt.php';
+$messages['bg'] = require __DIR__ . '/conditions_utilisation/bg.php';
+$messages['hr'] = require __DIR__ . '/conditions_utilisation/hr.php';
+$messages['cs'] = require __DIR__ . '/conditions_utilisation/cs.php';
+$messages['da'] = require __DIR__ . '/conditions_utilisation/da.php';
+$messages['et'] = require __DIR__ . '/conditions_utilisation/et.php';
+$messages['fi'] = require __DIR__ . '/conditions_utilisation/fi.php';
+$messages['el'] = require __DIR__ . '/conditions_utilisation/el.php';
+$messages['hu'] = require __DIR__ . '/conditions_utilisation/hu.php';
+$messages['ga'] = require __DIR__ . '/conditions_utilisation/ga.php';
+$messages['lv'] = require __DIR__ . '/conditions_utilisation/lv.php';
+$messages['lt'] = require __DIR__ . '/conditions_utilisation/lt.php';
+$messages['mt'] = require __DIR__ . '/conditions_utilisation/mt.php';
+$messages['pl'] = require __DIR__ . '/conditions_utilisation/pl.php';
+$messages['ro'] = require __DIR__ . '/conditions_utilisation/ro.php';
+$messages['sk'] = require __DIR__ . '/conditions_utilisation/sk.php';
+$messages['sl'] = require __DIR__ . '/conditions_utilisation/sl.php';
+$messages['sv'] = require __DIR__ . '/conditions_utilisation/sv.php';
+$messages['ar'] = require __DIR__ . '/conditions_utilisation/ar.php';
+$messages['hi'] = require __DIR__ . '/conditions_utilisation/hi.php';
+$messages['ja'] = require __DIR__ . '/conditions_utilisation/ja.php';
+$messages['zh'] = require __DIR__ . '/conditions_utilisation/zh.php';
+$messages['bn'] = require __DIR__ . '/conditions_utilisation/bn.php';
+$messages['ru'] = require __DIR__ . '/conditions_utilisation/ru.php';
+$messages['id'] = require __DIR__ . '/conditions_utilisation/id.php';
+
+return $messages;
