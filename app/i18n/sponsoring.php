571e2709e7749eb789266db0adc4688ef82c8f5e
odebauche <odebauche@awegroupe.be>
2026-05-31 08:24:09 +0200

    improvement phase IV

--- app/i18n/sponsoring.php
+++ app/i18n/sponsoring.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/sponsoring/fr.php',
-    'en' => require __DIR__ . '/sponsoring/en.php',
-    'de' => require __DIR__ . '/sponsoring/de.php',
-    'nl' => require __DIR__ . '/sponsoring/nl.php',
-    'it' => require __DIR__ . '/sponsoring/it.php',
-    'es' => require __DIR__ . '/sponsoring/es.php',
-    'pt' => require __DIR__ . '/sponsoring/pt.php',
-    'ar' => require __DIR__ . '/sponsoring/ar.php',
-    'hi' => require __DIR__ . '/sponsoring/hi.php',
-    'ja' => require __DIR__ . '/sponsoring/ja.php',
-    'zh' => require __DIR__ . '/sponsoring/zh.php',
-    'bn' => require __DIR__ . '/sponsoring/bn.php',
-    'ru' => require __DIR__ . '/sponsoring/ru.php',
-    'id' => require __DIR__ . '/sponsoring/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/sponsoring/fr.php';
+$messages['en'] = require __DIR__ . '/sponsoring/en.php';
+$messages['de'] = require __DIR__ . '/sponsoring/de.php';
+$messages['nl'] = require __DIR__ . '/sponsoring/nl.php';
+$messages['it'] = require __DIR__ . '/sponsoring/it.php';
+$messages['es'] = require __DIR__ . '/sponsoring/es.php';
+$messages['pt'] = require __DIR__ . '/sponsoring/pt.php';
+$messages['bg'] = require __DIR__ . '/sponsoring/bg.php';
+$messages['hr'] = require __DIR__ . '/sponsoring/hr.php';
+$messages['cs'] = require __DIR__ . '/sponsoring/cs.php';
+$messages['da'] = require __DIR__ . '/sponsoring/da.php';
+$messages['et'] = require __DIR__ . '/sponsoring/et.php';
+$messages['fi'] = require __DIR__ . '/sponsoring/fi.php';
+$messages['el'] = require __DIR__ . '/sponsoring/el.php';
+$messages['hu'] = require __DIR__ . '/sponsoring/hu.php';
+$messages['ga'] = require __DIR__ . '/sponsoring/ga.php';
+$messages['lv'] = require __DIR__ . '/sponsoring/lv.php';
+$messages['lt'] = require __DIR__ . '/sponsoring/lt.php';
+$messages['mt'] = require __DIR__ . '/sponsoring/mt.php';
+$messages['pl'] = require __DIR__ . '/sponsoring/pl.php';
+$messages['ro'] = require __DIR__ . '/sponsoring/ro.php';
+$messages['sk'] = require __DIR__ . '/sponsoring/sk.php';
+$messages['sl'] = require __DIR__ . '/sponsoring/sl.php';
+$messages['sv'] = require __DIR__ . '/sponsoring/sv.php';
+$messages['ar'] = require __DIR__ . '/sponsoring/ar.php';
+$messages['hi'] = require __DIR__ . '/sponsoring/hi.php';
+$messages['ja'] = require __DIR__ . '/sponsoring/ja.php';
+$messages['zh'] = require __DIR__ . '/sponsoring/zh.php';
+$messages['bn'] = require __DIR__ . '/sponsoring/bn.php';
+$messages['ru'] = require __DIR__ . '/sponsoring/ru.php';
+$messages['id'] = require __DIR__ . '/sponsoring/id.php';
+
+return $messages;
