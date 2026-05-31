bd2d997b0efdf98177db66cf4059d4629af60711
odebauche <odebauche@awegroupe.be>
2026-05-31 08:18:24 +0200

    improvement phase IV

--- app/i18n/reglement_interieur.php
+++ app/i18n/reglement_interieur.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/reglement_interieur/fr.php',
-    'en' => require __DIR__ . '/reglement_interieur/en.php',
-    'de' => require __DIR__ . '/reglement_interieur/de.php',
-    'nl' => require __DIR__ . '/reglement_interieur/nl.php',
-    'it' => require __DIR__ . '/reglement_interieur/it.php',
-    'es' => require __DIR__ . '/reglement_interieur/es.php',
-    'pt' => require __DIR__ . '/reglement_interieur/pt.php',
-    'ar' => require __DIR__ . '/reglement_interieur/ar.php',
-    'hi' => require __DIR__ . '/reglement_interieur/hi.php',
-    'ja' => require __DIR__ . '/reglement_interieur/ja.php',
-    'zh' => require __DIR__ . '/reglement_interieur/zh.php',
-    'bn' => require __DIR__ . '/reglement_interieur/bn.php',
-    'ru' => require __DIR__ . '/reglement_interieur/ru.php',
-    'id' => require __DIR__ . '/reglement_interieur/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/reglement_interieur/fr.php';
+$messages['en'] = require __DIR__ . '/reglement_interieur/en.php';
+$messages['de'] = require __DIR__ . '/reglement_interieur/de.php';
+$messages['nl'] = require __DIR__ . '/reglement_interieur/nl.php';
+$messages['it'] = require __DIR__ . '/reglement_interieur/it.php';
+$messages['es'] = require __DIR__ . '/reglement_interieur/es.php';
+$messages['pt'] = require __DIR__ . '/reglement_interieur/pt.php';
+$messages['bg'] = require __DIR__ . '/reglement_interieur/bg.php';
+$messages['hr'] = require __DIR__ . '/reglement_interieur/hr.php';
+$messages['cs'] = require __DIR__ . '/reglement_interieur/cs.php';
+$messages['da'] = require __DIR__ . '/reglement_interieur/da.php';
+$messages['et'] = require __DIR__ . '/reglement_interieur/et.php';
+$messages['fi'] = require __DIR__ . '/reglement_interieur/fi.php';
+$messages['el'] = require __DIR__ . '/reglement_interieur/el.php';
+$messages['hu'] = require __DIR__ . '/reglement_interieur/hu.php';
+$messages['ga'] = require __DIR__ . '/reglement_interieur/ga.php';
+$messages['lv'] = require __DIR__ . '/reglement_interieur/lv.php';
+$messages['lt'] = require __DIR__ . '/reglement_interieur/lt.php';
+$messages['mt'] = require __DIR__ . '/reglement_interieur/mt.php';
+$messages['pl'] = require __DIR__ . '/reglement_interieur/pl.php';
+$messages['ro'] = require __DIR__ . '/reglement_interieur/ro.php';
+$messages['sk'] = require __DIR__ . '/reglement_interieur/sk.php';
+$messages['sl'] = require __DIR__ . '/reglement_interieur/sl.php';
+$messages['sv'] = require __DIR__ . '/reglement_interieur/sv.php';
+$messages['ar'] = require __DIR__ . '/reglement_interieur/ar.php';
+$messages['hi'] = require __DIR__ . '/reglement_interieur/hi.php';
+$messages['ja'] = require __DIR__ . '/reglement_interieur/ja.php';
+$messages['zh'] = require __DIR__ . '/reglement_interieur/zh.php';
+$messages['bn'] = require __DIR__ . '/reglement_interieur/bn.php';
+$messages['ru'] = require __DIR__ . '/reglement_interieur/ru.php';
+$messages['id'] = require __DIR__ . '/reglement_interieur/id.php';
+
+return $messages;
