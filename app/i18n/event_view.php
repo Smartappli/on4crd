4f86eb8c86617ead71685f01cf5d5c8f27c5d065
odebauche <odebauche@awegroupe.be>
2026-05-31 07:58:41 +0200

    improvement phase IV

--- app/i18n/event_view.php
+++ app/i18n/event_view.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/event_view/fr.php',
-    'en' => require __DIR__ . '/event_view/en.php',
-    'de' => require __DIR__ . '/event_view/de.php',
-    'nl' => require __DIR__ . '/event_view/nl.php',
-    'it' => require __DIR__ . '/event_view/it.php',
-    'es' => require __DIR__ . '/event_view/es.php',
-    'pt' => require __DIR__ . '/event_view/pt.php',
-    'ar' => require __DIR__ . '/event_view/ar.php',
-    'hi' => require __DIR__ . '/event_view/hi.php',
-    'ja' => require __DIR__ . '/event_view/ja.php',
-    'zh' => require __DIR__ . '/event_view/zh.php',
-    'bn' => require __DIR__ . '/event_view/bn.php',
-    'ru' => require __DIR__ . '/event_view/ru.php',
-    'id' => require __DIR__ . '/event_view/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/event_view/fr.php';
+$messages['en'] = require __DIR__ . '/event_view/en.php';
+$messages['de'] = require __DIR__ . '/event_view/de.php';
+$messages['nl'] = require __DIR__ . '/event_view/nl.php';
+$messages['it'] = require __DIR__ . '/event_view/it.php';
+$messages['es'] = require __DIR__ . '/event_view/es.php';
+$messages['pt'] = require __DIR__ . '/event_view/pt.php';
+$messages['bg'] = require __DIR__ . '/event_view/bg.php';
+$messages['hr'] = require __DIR__ . '/event_view/hr.php';
+$messages['cs'] = require __DIR__ . '/event_view/cs.php';
+$messages['da'] = require __DIR__ . '/event_view/da.php';
+$messages['et'] = require __DIR__ . '/event_view/et.php';
+$messages['fi'] = require __DIR__ . '/event_view/fi.php';
+$messages['el'] = require __DIR__ . '/event_view/el.php';
+$messages['hu'] = require __DIR__ . '/event_view/hu.php';
+$messages['ga'] = require __DIR__ . '/event_view/ga.php';
+$messages['lv'] = require __DIR__ . '/event_view/lv.php';
+$messages['lt'] = require __DIR__ . '/event_view/lt.php';
+$messages['mt'] = require __DIR__ . '/event_view/mt.php';
+$messages['pl'] = require __DIR__ . '/event_view/pl.php';
+$messages['ro'] = require __DIR__ . '/event_view/ro.php';
+$messages['sk'] = require __DIR__ . '/event_view/sk.php';
+$messages['sl'] = require __DIR__ . '/event_view/sl.php';
+$messages['sv'] = require __DIR__ . '/event_view/sv.php';
+$messages['ar'] = require __DIR__ . '/event_view/ar.php';
+$messages['hi'] = require __DIR__ . '/event_view/hi.php';
+$messages['ja'] = require __DIR__ . '/event_view/ja.php';
+$messages['zh'] = require __DIR__ . '/event_view/zh.php';
+$messages['bn'] = require __DIR__ . '/event_view/bn.php';
+$messages['ru'] = require __DIR__ . '/event_view/ru.php';
+$messages['id'] = require __DIR__ . '/event_view/id.php';
+
+return $messages;
--- app/i18n/events.php
+++ app/i18n/events.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/events/fr.php',
-    'en' => require __DIR__ . '/events/en.php',
-    'de' => require __DIR__ . '/events/de.php',
-    'nl' => require __DIR__ . '/events/nl.php',
-    'es' => require __DIR__ . '/events/es.php',
-    'it' => require __DIR__ . '/events/it.php',
-    'pt' => require __DIR__ . '/events/pt.php',
-    'ar' => require __DIR__ . '/events/ar.php',
-    'hi' => require __DIR__ . '/events/hi.php',
-    'ja' => require __DIR__ . '/events/ja.php',
-    'zh' => require __DIR__ . '/events/zh.php',
-    'bn' => require __DIR__ . '/events/bn.php',
-    'ru' => require __DIR__ . '/events/ru.php',
-    'id' => require __DIR__ . '/events/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/events/fr.php';
+$messages['en'] = require __DIR__ . '/events/en.php';
+$messages['de'] = require __DIR__ . '/events/de.php';
+$messages['nl'] = require __DIR__ . '/events/nl.php';
+$messages['it'] = require __DIR__ . '/events/it.php';
+$messages['es'] = require __DIR__ . '/events/es.php';
+$messages['pt'] = require __DIR__ . '/events/pt.php';
+$messages['bg'] = require __DIR__ . '/events/bg.php';
+$messages['hr'] = require __DIR__ . '/events/hr.php';
+$messages['cs'] = require __DIR__ . '/events/cs.php';
+$messages['da'] = require __DIR__ . '/events/da.php';
+$messages['et'] = require __DIR__ . '/events/et.php';
+$messages['fi'] = require __DIR__ . '/events/fi.php';
+$messages['el'] = require __DIR__ . '/events/el.php';
+$messages['hu'] = require __DIR__ . '/events/hu.php';
+$messages['ga'] = require __DIR__ . '/events/ga.php';
+$messages['lv'] = require __DIR__ . '/events/lv.php';
+$messages['lt'] = require __DIR__ . '/events/lt.php';
+$messages['mt'] = require __DIR__ . '/events/mt.php';
+$messages['pl'] = require __DIR__ . '/events/pl.php';
+$messages['ro'] = require __DIR__ . '/events/ro.php';
+$messages['sk'] = require __DIR__ . '/events/sk.php';
+$messages['sl'] = require __DIR__ . '/events/sl.php';
+$messages['sv'] = require __DIR__ . '/events/sv.php';
+$messages['ar'] = require __DIR__ . '/events/ar.php';
+$messages['hi'] = require __DIR__ . '/events/hi.php';
+$messages['ja'] = require __DIR__ . '/events/ja.php';
+$messages['zh'] = require __DIR__ . '/events/zh.php';
+$messages['bn'] = require __DIR__ . '/events/bn.php';
+$messages['ru'] = require __DIR__ . '/events/ru.php';
+$messages['id'] = require __DIR__ . '/events/id.php';
+
+return $messages;
