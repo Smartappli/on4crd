c95ec9c4bfd949442936407a3ade79f9c6c5be33
odebauche <odebauche@awegroupe.be>
2026-05-31 07:44:05 +0200

    improvement phase IV

--- app/i18n/admin_permissions.php
+++ app/i18n/admin_permissions.php
@@ -1,17 +1,37 @@
 <?php
-return [
-    'fr' => require __DIR__ . '/admin_permissions/fr.php',
-    'en' => require __DIR__ . '/admin_permissions/en.php',
-    'de' => require __DIR__ . '/admin_permissions/de.php',
-    'nl' => require __DIR__ . '/admin_permissions/nl.php',
-    'es' => require __DIR__ . '/admin_permissions/es.php',
-    'it' => require __DIR__ . '/admin_permissions/it.php',
-    'pt' => require __DIR__ . '/admin_permissions/pt.php',
-    'ar' => require __DIR__ . '/admin_permissions/ar.php',
-    'hi' => require __DIR__ . '/admin_permissions/hi.php',
-    'ja' => require __DIR__ . '/admin_permissions/ja.php',
-    'zh' => require __DIR__ . '/admin_permissions/zh.php',
-    'bn' => require __DIR__ . '/admin_permissions/bn.php',
-    'ru' => require __DIR__ . '/admin_permissions/ru.php',
-    'id' => require __DIR__ . '/admin_permissions/id.php',
-];
+declare(strict_types=1);
+
+$messages = [];
+$messages['fr'] = require __DIR__ . '/admin_permissions/fr.php';
+$messages['en'] = require __DIR__ . '/admin_permissions/en.php';
+$messages['de'] = require __DIR__ . '/admin_permissions/de.php';
+$messages['nl'] = require __DIR__ . '/admin_permissions/nl.php';
+$messages['it'] = require __DIR__ . '/admin_permissions/it.php';
+$messages['es'] = require __DIR__ . '/admin_permissions/es.php';
+$messages['pt'] = require __DIR__ . '/admin_permissions/pt.php';
+$messages['bg'] = require __DIR__ . '/admin_permissions/bg.php';
+$messages['hr'] = require __DIR__ . '/admin_permissions/hr.php';
+$messages['cs'] = require __DIR__ . '/admin_permissions/cs.php';
+$messages['da'] = require __DIR__ . '/admin_permissions/da.php';
+$messages['et'] = require __DIR__ . '/admin_permissions/et.php';
+$messages['fi'] = require __DIR__ . '/admin_permissions/fi.php';
+$messages['el'] = require __DIR__ . '/admin_permissions/el.php';
+$messages['hu'] = require __DIR__ . '/admin_permissions/hu.php';
+$messages['ga'] = require __DIR__ . '/admin_permissions/ga.php';
+$messages['lv'] = require __DIR__ . '/admin_permissions/lv.php';
+$messages['lt'] = require __DIR__ . '/admin_permissions/lt.php';
+$messages['mt'] = require __DIR__ . '/admin_permissions/mt.php';
+$messages['pl'] = require __DIR__ . '/admin_permissions/pl.php';
+$messages['ro'] = require __DIR__ . '/admin_permissions/ro.php';
+$messages['sk'] = require __DIR__ . '/admin_permissions/sk.php';
+$messages['sl'] = require __DIR__ . '/admin_permissions/sl.php';
+$messages['sv'] = require __DIR__ . '/admin_permissions/sv.php';
+$messages['ar'] = require __DIR__ . '/admin_permissions/ar.php';
+$messages['hi'] = require __DIR__ . '/admin_permissions/hi.php';
+$messages['ja'] = require __DIR__ . '/admin_permissions/ja.php';
+$messages['zh'] = require __DIR__ . '/admin_permissions/zh.php';
+$messages['bn'] = require __DIR__ . '/admin_permissions/bn.php';
+$messages['ru'] = require __DIR__ . '/admin_permissions/ru.php';
+$messages['id'] = require __DIR__ . '/admin_permissions/id.php';
+
+return $messages;
