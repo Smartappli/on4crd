03e409fdbc2667a378a1baf3e2de92328e43b2e5
odebauche <odebauche@awegroupe.be>
2026-05-31 07:44:40 +0200

    improvement phase IV

--- app/i18n/admin_press.php
+++ app/i18n/admin_press.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'fr' => require __DIR__ . '/admin_press/fr.php',
-    'en' => require __DIR__ . '/admin_press/en.php',
-    'de' => require __DIR__ . '/admin_press/de.php',
-    'nl' => require __DIR__ . '/admin_press/nl.php',
-    'es' => require __DIR__ . '/admin_press/es.php',
-    'it' => require __DIR__ . '/admin_press/it.php',
-    'pt' => require __DIR__ . '/admin_press/pt.php',
-    'ar' => require __DIR__ . '/admin_press/ar.php',
-    'hi' => require __DIR__ . '/admin_press/hi.php',
-    'ja' => require __DIR__ . '/admin_press/ja.php',
-    'zh' => require __DIR__ . '/admin_press/zh.php',
-    'bn' => require __DIR__ . '/admin_press/bn.php',
-    'ru' => require __DIR__ . '/admin_press/ru.php',
-    'id' => require __DIR__ . '/admin_press/id.php',
-];
+$messages = [];
+$messages['fr'] = require __DIR__ . '/admin_press/fr.php';
+$messages['en'] = require __DIR__ . '/admin_press/en.php';
+$messages['de'] = require __DIR__ . '/admin_press/de.php';
+$messages['nl'] = require __DIR__ . '/admin_press/nl.php';
+$messages['it'] = require __DIR__ . '/admin_press/it.php';
+$messages['es'] = require __DIR__ . '/admin_press/es.php';
+$messages['pt'] = require __DIR__ . '/admin_press/pt.php';
+$messages['bg'] = require __DIR__ . '/admin_press/bg.php';
+$messages['hr'] = require __DIR__ . '/admin_press/hr.php';
+$messages['cs'] = require __DIR__ . '/admin_press/cs.php';
+$messages['da'] = require __DIR__ . '/admin_press/da.php';
+$messages['et'] = require __DIR__ . '/admin_press/et.php';
+$messages['fi'] = require __DIR__ . '/admin_press/fi.php';
+$messages['el'] = require __DIR__ . '/admin_press/el.php';
+$messages['hu'] = require __DIR__ . '/admin_press/hu.php';
+$messages['ga'] = require __DIR__ . '/admin_press/ga.php';
+$messages['lv'] = require __DIR__ . '/admin_press/lv.php';
+$messages['lt'] = require __DIR__ . '/admin_press/lt.php';
+$messages['mt'] = require __DIR__ . '/admin_press/mt.php';
+$messages['pl'] = require __DIR__ . '/admin_press/pl.php';
+$messages['ro'] = require __DIR__ . '/admin_press/ro.php';
+$messages['sk'] = require __DIR__ . '/admin_press/sk.php';
+$messages['sl'] = require __DIR__ . '/admin_press/sl.php';
+$messages['sv'] = require __DIR__ . '/admin_press/sv.php';
+$messages['ar'] = require __DIR__ . '/admin_press/ar.php';
+$messages['hi'] = require __DIR__ . '/admin_press/hi.php';
+$messages['ja'] = require __DIR__ . '/admin_press/ja.php';
+$messages['zh'] = require __DIR__ . '/admin_press/zh.php';
+$messages['bn'] = require __DIR__ . '/admin_press/bn.php';
+$messages['ru'] = require __DIR__ . '/admin_press/ru.php';
+$messages['id'] = require __DIR__ . '/admin_press/id.php';
+
+return $messages;
