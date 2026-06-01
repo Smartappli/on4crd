1f8ec8c5e18c77bcd700f98d59a0568069869087
odebauche <odebauche@awegroupe.be>
2026-06-01 19:35:05 +0200

    improvement phase IV

--- pages/settings.php
+++ pages/settings.php
@@ -34,25 +34,9 @@ $recommendationSignals = [
 ];
 $recommendations = $recommendationsEnabled ? member_personalized_recommendations($userId, 6) : [];
 
-$links = [
-    ['route' => 'code_q', 'label_key' => 'link_code_q'],
-    ['route' => 'code_cw', 'label_key' => 'link_code_cw'],
-    ['route' => 'bandplan_on3', 'label_key' => 'link_bandplan_on3'],
-    ['route' => 'bandplan_on2', 'label_key' => 'link_bandplan_on2'],
-    ['route' => 'bandplan_harec', 'label_key' => 'link_bandplan_harec'],
-];
 $pageTitle = (string) ($t['title'] ?? 'Account settings');
 ob_start();
 ?>
-<section class="card">
-  <h1><?= e($pageTitle) ?></h1>
-  <p><?= e((string) ($t['intro'] ?? 'Manage your account preferences and interface options here.')) ?></p>
-  <ul>
-    <?php foreach ($links as $link): ?>
-      <li><a href="<?= e(route_url((string) $link['route'])) ?>"><?= e((string) ($t[(string) $link['label_key']] ?? $link['route'])) ?></a></li>
-    <?php endforeach; ?>
-  </ul>
-</section>
 <section class="card">
   <h2 style="margin-top:0;"><?= e($rt('recommendations_title')) ?></h2>
   <form method="post" class="inline-form" style="margin:.25rem 0 1rem;">
