d0b81fe7e392870f5386c5f43debfd7a7e63a57b
odebauche <odebauche@awegroupe.be>
2026-06-02 06:53:30 +0200

    classifieds improvement

---
+++ assets/js/modules/classifieds.js
@@ -0,0 +1,39 @@
+(function () {
+  const dialog = document.getElementById('classifieds-category-dialog');
+  if (!(dialog instanceof HTMLDialogElement)) {
+    return;
+  }
+
+  const openButtons = document.querySelectorAll('[data-classifieds-category-open]');
+  const closeButtons = dialog.querySelectorAll('[data-classifieds-category-close]');
+  const firstField = dialog.querySelector('input[name="proposal_category"]');
+
+  const openDialog = () => {
+    if (!dialog.open) {
+      dialog.showModal();
+    }
+    if (firstField instanceof HTMLElement) {
+      firstField.focus();
+    }
+  };
+
+  const closeDialog = () => {
+    if (dialog.open) {
+      dialog.close();
+    }
+  };
+
+  openButtons.forEach((button) => {
+    button.addEventListener('click', openDialog);
+  });
+
+  closeButtons.forEach((button) => {
+    button.addEventListener('click', closeDialog);
+  });
+
+  dialog.addEventListener('click', (event) => {
+    if (event.target === dialog) {
+      closeDialog();
+    }
+  });
+})();
