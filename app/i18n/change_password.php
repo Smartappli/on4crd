ea21a8ded9fb9ec45a657b96a00b713f24fc7d3f
odebauche <odebauche@awegroupe.be>
2026-06-04 20:36:07 +0200

    cosmetic changes

---
+++ app/i18n/change_password.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'fr' => require __DIR__ . '/change_password/fr.php',
+    'en' => require __DIR__ . '/change_password/en.php',
+];
---
+++ app/i18n/change_password/fr.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'title' => 'Modification du mot de passe',
+    'intro_required' => 'Pour continuer, vous devez modifier le mot de passe créé lors de l’inscription.',
+    'intro_optional' => 'Vous pouvez modifier votre mot de passe ici.',
+    'current_password' => 'Mot de passe actuel',
+    'new_password' => 'Nouveau mot de passe',
+    'confirm_password' => 'Confirmer le nouveau mot de passe',
+    'submit' => 'Enregistrer le nouveau mot de passe',
+    'err_required' => 'Tous les champs sont obligatoires.',
+    'err_mismatch' => 'Les deux nouveaux mots de passe ne correspondent pas.',
+    'err_same_password' => 'Le nouveau mot de passe doit être différent du mot de passe actuel.',
+    'err_invalid_password' => 'Mot de passe actuel incorrect ou nouveau mot de passe invalide (minimum 8 caractères recommandé).',
+    'err_auth_unavailable' => 'Module d’authentification indisponible. Lancez composer install.',
+    'err_not_logged_in' => 'Veuillez vous connecter pour modifier votre mot de passe.',
+    'err_too_many' => 'Trop de tentatives. Réessayez dans quelques minutes.',
+    'ok_updated' => 'Mot de passe modifié. Vous pouvez continuer.',
+    'forced_notice' => 'Veuillez modifier votre mot de passe pour continuer.',
+    'layout_title' => 'Modification du mot de passe',
+];
