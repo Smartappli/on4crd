c16f9fbbad693acd4fe0c0e614c6f16664740c06
odebauche <odebauche@awegroupe.be>
2026-06-05 20:36:50 +0200

    cosmetic changes

--- pages/bandplan_on2.php
+++ pages/bandplan_on2.php
@@ -2,37 +2,19 @@
 declare(strict_types=1);
 
 $locale = current_locale();
-
-$titleMap = [
-    'fr' => 'ON2 band plan',
-    'en' => 'ON2 band plan',
-    'de' => 'ON2 band plan',
-    'nl' => 'ON2 band plan',
-    'es' => 'ON2 band plan',
-    'it' => 'ON2 band plan',
-    'pt' => 'ON2 band plan',
-    'ar' => 'ON2 band plan',
-    'hi' => 'ON2 band plan',
-    'ja' => 'ON2 band plan',
-    'zh' => 'ON2 band plan',
-    'bn' => 'ON2 band plan',
-    'ru' => 'ON2 band plan',
-    'id' => 'ON2 band plan',
-];
-$title = i18n_localized_value($titleMap, $locale, 'en');
-
-$headers = ['band' => 'Band', 'freq' => 'Frequencies (MHz)', 'modes' => 'Modes', 'pwr' => 'Max power*', 'notes' => 'Notes'];
+$t = i18n_domain_locale('bandplan_on2', $locale);
+$title = (string) $t['title'];
 $rows = [
-    ['160 m', '1.810-2.000', 'CW/SSB/Data', 'Follow IARU R1 segmentation.', '100 W PEP'],
-    ['80 m', '3.500-3.800', 'CW/SSB/Data', 'Phone segment in upper band.', '100 W PEP'],
-    ['40 m', '7.000-7.200', 'CW/SSB/Data', 'Frequent contest usage.', '100 W PEP'],
-    ['20 m', '14.000-14.350', 'CW/SSB/Data', 'FT8 commonly on 14.074 MHz.', '100 W PEP'],
-    ['15 m', '21.000-21.450', 'CW/SSB/Data', 'Variable daytime propagation.', '100 W PEP'],
-    ['10 m', '28.000-29.700', 'CW/SSB/FM/Data', 'Includes beacons and FM sub-band.', '100 W PEP'],
-    ['6 m', '50.000-52.000', 'CW/SSB/FM/Data', 'Sporadic-E openings possible.', '100 W PEP'],
-    ['2 m', '144.000-146.000', 'FM/SSB/CW/Data', 'Simplex calling on 145.500 MHz.', '50 W PEP'],
-    ['70 cm', '430.000-440.000', 'FM/SSB/CW/DV', 'Respect local repeater coordination.', '50 W PEP'],
-    ['23 cm', '1240-1300', 'FM/SSB/CW/Data', 'Shared band depending on local usage.', '50 W PEP'],
+    ['160 m', '1.810-2.000', 'CW/SSB/Data', '100 W PEP', 'note_160m'],
+    ['80 m', '3.500-3.800', 'CW/SSB/Data', '100 W PEP', 'note_80m'],
+    ['40 m', '7.000-7.200', 'CW/SSB/Data', '100 W PEP', 'note_40m'],
+    ['20 m', '14.000-14.350', 'CW/SSB/Data', '100 W PEP', 'note_20m'],
+    ['15 m', '21.000-21.450', 'CW/SSB/Data', '100 W PEP', 'note_15m'],
+    ['10 m', '28.000-29.700', 'CW/SSB/FM/Data', '100 W PEP', 'note_10m'],
+    ['6 m', '50.000-52.000', 'CW/SSB/FM/Data', '100 W PEP', 'note_6m'],
+    ['2 m', '144.000-146.000', 'FM/SSB/CW/Data', '50 W PEP', 'note_2m'],
+    ['70 cm', '430.000-440.000', 'FM/SSB/CW/DV', '50 W PEP', 'note_70cm'],
+    ['23 cm', '1240-1300', 'FM/SSB/CW/Data', '50 W PEP', 'note_23cm'],
 ];
 
 ob_start();
@@ -44,11 +26,11 @@ ob_start();
         <table>
             <thead>
             <tr>
-                <th><?= e($headers['band']) ?></th>
-                <th><?= e($headers['freq']) ?></th>
-                <th><?= e($headers['modes']) ?></th>
-                <th><?= e($headers['pwr']) ?></th>
-                <th><?= e($headers['notes']) ?></th>
+                <th><?= e((string) $t['header_band']) ?></th>
+                <th><?= e((string) $t['header_freq']) ?></th>
+                <th><?= e((string) $t['header_modes']) ?></th>
+                <th><?= e((string) $t['header_power']) ?></th>
+                <th><?= e((string) $t['header_notes']) ?></th>
             </tr>
             </thead>
             <tbody>
@@ -57,8 +39,8 @@ ob_start();
                     <td><?= e($r[0]) ?></td>
                     <td><?= e($r[1]) ?></td>
                     <td><?= e($r[2]) ?></td>
-                    <td><?= e($r[4]) ?></td>
                     <td><?= e($r[3]) ?></td>
+                    <td><?= e((string) ($t[(string) $r[4]] ?? '')) ?></td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
