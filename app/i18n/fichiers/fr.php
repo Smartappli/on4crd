75e000070b3fbb0aeb71271bf73d794e10540ea7
odebauche <odebauche@awegroupe.be>
2026-06-13 20:22:16 +0200

    new modules

--- app/i18n/fichiers/fr.php
+++ app/i18n/fichiers/fr.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 
 return array (
   'title' => 'Fichiers',
-  'intro' => 'Fichiers et ressources a telecharger pour les membres.',
+  'intro' => 'Fichiers et ressources à télécharger pour les membres.',
   'meta_desc' => 'Fichiers reserves aux membres ON4CRD.',
 );
